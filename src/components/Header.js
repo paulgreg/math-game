5a54bd91ec7d8af7d9780cc527384c628fdc2079
Grégory Paul <paulgreg@gmail.com>
2021-01-16 14:50:04 +0100

    Rename to math-game

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,7 +11,7 @@ export default function Header() {
                 className="header-logo"
                 alt=""
             />
-            ️ Multiply Game
+            ️ Math Game
         </header>
     )
 }
