6219210263785f840b3d19ef8ee0bb578a0ba0cc
Grégory Paul <paulgreg@gmail.com>
2021-01-16 11:01:13 +0100

    Add colors according time

--- src/components/Chronometer.js
+++ src/components/Chronometer.js
@@ -1,6 +1,13 @@
 import './Chronometer.css'
 import { useEffect, useState } from 'react'
 
+function getClassNameAccordintToTime(t) {
+    if (t < 3) return 'fast'
+    if (t < 6) return 'medium'
+    if (t < 9) return 'long'
+    return 'verylong'
+}
+
 export default function Chronometer({ x, y, show }) {
     const [initial, setInitial] = useState(Date.now())
     const [lastTime, setLastTime] = useState()
@@ -19,11 +26,25 @@ export default function Chronometer({ x, y, show }) {
     useEffect(() => {
         setInitial(Date.now())
     }, [x, y])
+    if (!lastTime) return null
 
-    return lastTime ? (
+    const time = lastTime.toFixed(1)
+    const average = (times / count).toFixed(1)
+
+    return (
         <div className="chronometer">
-            <div>Time : {lastTime.toFixed(1)} sec.</div>
-            <div>Average : {(times / count).toFixed(1)} sec.</div>
+            <div>
+                Time :{' '}
+                <span className={getClassNameAccordintToTime(time)}>
+                    {time} sec.
+                </span>
+            </div>
+            <div>
+                Average :{' '}
+                <span className={getClassNameAccordintToTime(average)}>
+                    {average} sec.
+                </span>
+            </div>
         </div>
-    ) : null
+    )
 }
